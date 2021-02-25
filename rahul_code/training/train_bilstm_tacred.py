d5bb44d012c00c36edc16e710dd79b8165ed14e3
Rahul-Khanna <rkhanna712@gmail.com>
2021-02-24 18:41:02 -0800

    took out weight decay

--- rahul_code/training/train_bilstm_tacred.py
+++ rahul_code/training/train_bilstm_tacred.py
@@ -175,7 +175,7 @@ def main():
     if args.use_adagrad:
         optimizer = Adagrad(clf.parameters(), lr=args.learning_rate)
     else:
-        optimizer = AdamW(clf.parameters(), lr=args.learning_rate, weight_decay=0.5)
+        optimizer = AdamW(clf.parameters(), lr=args.learning_rate)
     
     # define loss functions
     strict_match_loss_function  = nn.CrossEntropyLoss()
